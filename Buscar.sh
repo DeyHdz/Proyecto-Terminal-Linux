79440c4057b74a58c326c49e073f9bf7acd6243d
“Dey” <emailtouch>
2025-04-19 19:34:54 -0600

    [Deissy][Buscar.sh][Se agrgego diseño a la vista de busqueda]

--- Buscar.sh
+++ Buscar.sh
@@ -1,26 +1,31 @@
-
-
 #!/bin/bash
 
-# Colores
-G='\033[0;32m' # Verde
-Y='\033[1;33m' # Amarillo
-R='\033[0;31m' # Rojo
-W='\033[0m'    # Reset color
+# Colores estándar
+R='\033[1;31m'   # Rojo brillante
+G='\033[1;32m'   # Verde brillante
+Y='\033[1;33m'   # Amarillo brillante
+B='\033[1;34m'   # Azul brillante
+M='\033[1;35m'   # Magenta brillante
+C='\033[1;36m'   # Cian brillante
+W='\033[1;37m'   # Blanco brillante (gris claro)
 
+clear
+echo -e "${M}"
+echo -e "\t\t▗▄▄▖ ▗▖ ▗▖ ▗▄▄▖ ▗▄▄▖ ▗▄▖ ▗▄▄▖ "
+echo -e "\t\t▐▌ ▐▌▐▌ ▐▌▐▌   ▐▌   ▐▌ ▐▌▐▌ ▐▌"
+echo -e "\t\t▐▛▀▚▖▐▌ ▐▌ ▝▀▚▖▐▌   ▐▛▀▜▌▐▛▀▚▖"
+echo -e "\t\t▐▙▄▞▘▝▚▄▞▘▗▄▄▞▘▝▚▄▄▖▐▌ ▐▌▐▌ ▐▌"
 
-
-
-
+echo -e "-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-"
 
 
 # Pedir nombre de la carpeta
-echo -e "\n Ingresa el nombre de la carpeta donde buscar:"
-read -p "Carpeta: " carpeta
+echo -e "${C}\n\t\t Ingresa el nombre de la carpeta donde buscar${W}"
+read -p "                     Carpeta: " carpeta
 
 # Pedir nombre del archivo
-echo -e "\n Ingresa el nombre del archivo que deseas buscar:"
-read -p "Archivo: " archivo
+echo -e "${C}\n\t\t Ingresa el nombre del archivo que deseas buscar${W}"
+read -p "                     Archivo: " archivo
 
 # Buscar la carpeta por nombre dentro de /home/$USER
 carpeta=$(find "/home/$USER" -type d -name "$carpeta" 2>/dev/null | head -n 1)
@@ -33,15 +38,13 @@ if [[ -d "$carpeta" ]]; then
     # Verificar si el archivo fue encontrado
     if [[ -f "$archivo" ]]; then
         
-        echo -e "\n ${G}Archivo encontrado${W} en:"
-        echo "$archivo"
+        echo -e "\n\t\t\t ${G}Archivo encontrado en:${W}"
+        echo -e "\t\t$archivo\n"
     else
         
-        echo -e "\n ${Y}No se encontró el archivo en la carpeta.${W}"
+        echo -e "\n\t\t\t${Y}No se encontró el archivo en la carpeta.${W}"
     fi
 else
 
-    echo -e "\n ${R}La carpeta no existe en el sistema.${W}"
+    echo -e "\n\t\t\t${R}La carpeta no existe en el sistema.${W}"
 fi
-
-
